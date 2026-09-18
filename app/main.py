aff80aa5ff148eab07f37e3216701e240ac33b58
Rob Hamilton <rgwhamilton@gmail.com>
2026-09-18 23:37:30 +0100

    Added CORS

--- app/main.py
+++ app/main.py
@@ -17,6 +17,16 @@ app = FastAPI(
     lifespan=lifespan
 )
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],        # allow all origins
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["Content-Type"],
+)
+
 @app.middleware("http")
 async def log_requests(request: Request, call_next):
     start=time.time()
